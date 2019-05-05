d5f3771c6958619369a56979543aed141153ccc0
Eric Londaits <eric.londaits@gmail.com>
2019-05-05 14:40:46 -0300

    Improves the color gradient.

--- src/es6/lib/rainbow.js
+++ src/es6/lib/rainbow.js
@@ -13,9 +13,12 @@ export default function rainbow(steps) {
   //   '#bc31ff',
   // ];
 
+  const minHue = 0;
+  const maxHue = 330;
   for (let i = 0; i < steps; i += 1) {
     answer.push(new paper.Color({
-      hue: (((steps - 1) - i) * (270 / steps)),
+      // hue: (((steps - 1) - i) * (270 / steps)),
+      hue: minHue + (maxHue - minHue) * (1 - (i / (steps - 1))),
       saturation: 1,
       brightness: 1,
       alpha: 1,
